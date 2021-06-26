174b39f928db651cf7e4a404bc62fdf408d84d93
David Phillips <david@acz.org>
2021-06-26 15:23:28 -0700

    Publish multi-architecture images

--- trino-docker.sh
+++ trino-docker.sh
@@ -5,9 +5,17 @@ set -eux
 VERSION=$1
 NAME=trinodb/trino
 IMAGE=trino:$VERSION
+TARGET=$NAME:$VERSION
 
 core/docker/build-remote.sh $VERSION
-docker tag $IMAGE $NAME:latest
-docker push $NAME:latest
-docker tag $IMAGE $NAME:$VERSION
-docker push $NAME:$VERSION
+
+docker tag $IMAGE-amd64 $TARGET-amd64
+docker tag $IMAGE-arm64 $TARGET-arm64
+docker push $TARGET-amd64
+docker push $TARGET-arm64
+
+docker manifest create $TARGET $TARGET-amd64 $TARGET-arm64
+docker manifest push --purge $TARGET
+
+docker manifest create $NAME:latest $TARGET-amd64 $TARGET-arm64
+docker manifest push --purge $NAME:latest
