77a234411ec44b7d7bf75b38e09905d829af628c
Jan Waś <jan.was@starburstdata.com>
2022-08-19 10:12:03 +0200

    Avoid duplication in docker script

--- trino-docker.sh
+++ trino-docker.sh
@@ -3,9 +3,9 @@
 set -eux
 
 VERSION=$1
-NAME=trinodb/trino
+REPO=trinodb/trino
 IMAGE=trino:$VERSION
-TARGET=$NAME:$VERSION
+TARGET=$REPO:$VERSION
 
 core/docker/build.sh -r "$VERSION"
 
@@ -14,8 +14,7 @@ docker tag "$IMAGE-arm64" "$TARGET-arm64"
 docker push "$TARGET-amd64"
 docker push "$TARGET-arm64"
 
-docker manifest create "$TARGET" "$TARGET-amd64" "$TARGET-arm64"
-docker manifest push --purge "$TARGET"
-
-docker manifest create "$NAME:latest" "$TARGET-amd64" "$TARGET-arm64"
-docker manifest push --purge "$NAME:latest"
+for name in "$TARGET" "$REPO:latest"; do
+    docker manifest create "$name" "$TARGET-amd64" "$TARGET-arm64"
+    docker manifest push --purge "$name"
+done
