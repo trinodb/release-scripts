f9b016caa2e7f23768e1b640f39a74b72fa60f43
Manfred Moser <manfred@simpligility.ca>
2023-11-17 12:55:52 -0800

    Remove github star banner

--- trino-docs.sh
+++ trino-docs.sh
@@ -42,70 +42,14 @@ find -H "$VERSION" -type f -name '*.html' -print0 | xargs -0 perl -pi -e \
       window._linkedin_data_partner_ids = ['$LINKEDIN_ID'];
     </script>
     <script async src=\"https://snap.licdn.com/li.lms-analytics/insight.min.js\"></script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
   </head>
 @;
 s@<style>
 @<link href=\"https://fonts.googleapis.com/css2?family=Alata&family=Open+Sans:ital,wght%400,300;0,400;0,600;1,400&display=swap\" rel=\"stylesheet\">
     <style>
 @;
-s@<header class=\"md-header\" data-md-component=\"header\">
-@<div id=\"announcement\">
-  <div id=\"announcement-content\">
-    <a href=\"https://github.com/trinodb/trino\" target=\"_blank\">
-      Do you ❤️ Trino? Give us a 🌟 on GitHub <i class=\"fab fa-github\"></i>
-    </a>
-  </div>
-</div>
-<header class=\"md-header\" data-md-component=\"header\">
-@;
-s@</header>@</header>@;
 s@<a href=\"[^\"]*\" title=\"Trino $VERSION Documentation\"@<a href=\"/\" title=\"Trino\"@;
 "
-
-cat <<EOT >>$VERSION/_static/trino.css
-
-.md-sidebar { padding-block-start: 75px; }
-.md-content { padding-block-start: 50px; }
-header.md-header { top: 53px; }
-#announcement {
-  z-index: 2;
-  position: fixed;
-  top: 0px;
-  width: 100%;
-}
-#announcement-content {
-  background-color: #dd00a1;
-  border: 1px;
-  color: white;
-  text-align: center;
-  height: 53px;
-  padding: 16px;
-  font-family: Alata, sans-serif;
-  line-height: 21.6px;
-  font-size: 18px;
-  font-weight: 300;
-  text-rendering: optimizeLegibility !important;
-  -webkit-font-smoothing: antialiased !important;
-}
-#announcement-content a {
-  margin-left: 32px;
-}
-.md-typeset span[id]:target:before {
-    margin-top:-92px;
-    padding-top:92px;
-}
-.md-typeset h1[id]:target:before,
-.md-typeset h2[id]:target:before,
-.md-typeset h3[id]:target:before,
-.md-typeset h4[id]:target:before,
-.md-typeset h5[id]:target:before,
-.md-typeset h6[id]:target:before,
-.md-typeset dl[id]:target:before {
-    margin-top:-134px;
-    padding-top:134px;
-}
-EOT
 
 echo "/current/* /$VERSION/:splat 200" >_redirects
 /bin/ln -sfh "$VERSION" current
